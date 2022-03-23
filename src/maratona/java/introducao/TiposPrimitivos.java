479ced0a1223f74f023899fb399c4887de030310
JefferonSilva <drankhar@gmail.com>
2022-03-23 17:58:13 -0300

    Tipos primitivos em java

---
+++ src/maratona/java/introducao/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package maratona.java.introducao;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Existe 8 Tipos primitivos no java 
+		 * int , double, float, char, byte, short, long, boolean
+		 */
+		
+		int idade = 2;
+		double salario = 5.45; 
+		float fruta = 55; 
+		char sexo = 'M';
+		byte senha = 125;
+		short m = 1587;
+		long d = 12589;
+		boolean ativo = true;
+		
+		
+		
+	}
+
+}
