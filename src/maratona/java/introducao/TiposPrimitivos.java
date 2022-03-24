372cd5657e4764574b91e1f26c18c85c3d83fa72
JefferonSilva <drankhar@gmail.com>
2022-03-24 09:35:11 -0300

    Correção convenções de variáveis

--- src/maratona/java/introducao/TiposPrimitivos.java
+++ src/maratona/java/introducao/TiposPrimitivos.java
@@ -9,16 +9,6 @@ public class TiposPrimitivos {
 		 * int , double, float, char, byte, short, long, boolean
 		 */
 		
-		int idade = 2;
-		double salario = 5.45; 
-		float fruta = 55; 
-		char sexo = 'M';
-		byte senha = 125;
-		short m = 1587;
-		long d = 12589;
-		boolean ativo = true;
-		
-		
 		
 	}
 
