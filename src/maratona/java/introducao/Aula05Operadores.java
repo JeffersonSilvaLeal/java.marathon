53b8a0aae7a5cf8824705d56f66b72d2c6f91e25
JefferonSilva <drankhar@gmail.com>
2022-03-28 11:50:27 -0300

    Operadores relacionais

--- src/maratona/java/introducao/Aula05Operadores.java
+++ src/maratona/java/introducao/Aula05Operadores.java
@@ -11,6 +11,7 @@ public class Aula05Operadores {
 		 * * multiplicação
 		 */
 		
+		System.out.println("Operadores básico");
 		int numero01 = 10;
 		int numero02 = 20;
 		
@@ -24,5 +25,41 @@ public class Aula05Operadores {
 		System.out.println("Resultado é: " + divisao);
 		System.out.println("Resultado é: " + multiplicacao);
 		
+		System.out.println("------------------------------");
+		System.out.println("Operadores lógicos");
+		/*
+		 * % Operador Resto
+		 * 
+		 */
+		
+		int resto = 20 % 2;
+		System.out.println("O Resto da divisão é: " + resto);
+		/**
+		 * Operador lógico sempre retorna número booleano
+		 * < menor
+		 * > maior
+		 * <= menor igual
+		 * >= maior igual
+		 * == igualdade
+		 * != diferente
+		 */	
+		
+		boolean isDezMaiorQueVinte = 10 > 20;
+		System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+		
+		boolean isDezMenorQueVinte = 10 < 20;
+		System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+		
+		boolean isDezMenorIgualVinte = 10 <= 20;
+		System.out.println("isDezMenorIgualVinte " + isDezMenorIgualVinte);
+		
+		boolean isDezMaiorIgualVinte = 10 >= 20;
+		System.out.println("isDezMaiorIgualVinte " + isDezMaiorIgualVinte);
+		
+		boolean isDezIgualVinte = 10 == 20;
+		System.out.println("isDezIgualVinte " + isDezIgualVinte);
+		
+		boolean isDezDiferentevinte = 10 != 20;
+		System.out.println("isDezDiferentevinte " + isDezDiferentevinte);
 	}
 }
