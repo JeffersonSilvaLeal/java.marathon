fe68179f4d8e6c1f5705da8f80598f01690418bd
JefferonSilva <drankhar@gmail.com>
2022-03-28 11:37:29 -0300

    Operadores Aritimeticos básico!!

---
+++ src/maratona/java/introducao/Aula05Operadores.java
@@ -0,0 +1,28 @@
+package maratona.java.introducao;
+
+public class Aula05Operadores {
+
+	public static void main(String[] args) {
+		/**
+		 * Operadores básicos
+		 * + soma
+		 * - subtração
+		 ** /divisão
+		 * * multiplicação
+		 */
+		
+		int numero01 = 10;
+		int numero02 = 20;
+		
+		int soma = numero01 + numero02;
+		int subtracao = numero01 - numero02;
+		int divisao = numero01 / numero02;
+		int multiplicacao = numero01 + numero02;
+		
+		System.out.println("Resultado é: " + soma);
+		System.out.println("Resultado e: " + subtracao);
+		System.out.println("Resultado é: " + divisao);
+		System.out.println("Resultado é: " + multiplicacao);
+		
+	}
+}
