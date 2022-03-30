3bf1a0a469e5d3ce9fc53d284f62fcc0b6d4363f
JefferonSilva <drankhar@gmail.com>
2022-03-30 14:24:57 -0300

    Tabela Verdade  && e ||

---
+++ src/maratona/java/estruturascondicionais/Aula06EstruturasCondicionais04.java
@@ -0,0 +1,35 @@
+package maratona.java.estruturascondicionais;
+
+public class Aula06EstruturasCondicionais04 {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * Tabela verdade &&
+		 * 
+		 * V && V = V
+		 * V && F = F
+		 * V && V = F
+		 * F && F = F
+		 * 
+		 * Será verdadeiro se tudo for verdadeiro
+		 * 
+		 */
+		
+		/**
+		 * Tabela verdade ||
+		 * 
+		 * V || V = V
+		 * V || F = V
+		 * F || V = V
+		 * F || F = F
+		 * 
+		 * Será falso se tudo for falso
+		 * 
+		 */
+		
+
+		
+	}
+
+}
