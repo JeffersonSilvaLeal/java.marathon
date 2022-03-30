0c823952da3513e8b45529472ee5613c9f390e84
JefferonSilva <drankhar@gmail.com>
2022-03-30 17:25:15 -0300

    Arrays pt 02

---
+++ src/maratona/java/arrays/Aula07Arrays02.java
@@ -0,0 +1,37 @@
+package maratona.java.arrays;
+
+public class Aula07Arrays02 {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Desclaração de arrys
+		 * byte, short, int, float e double  = 0
+		 * char = '\0000'  ' '
+		 * boolean = false
+		 * String =  null
+		 */
+		
+		byte[] idades = new byte[3];
+		System.out.println(idades[0]);
+		System.out.println(idades[1]);
+		System.out.println(idades[2]);
+		
+		char[] idades1 = new char[3];
+		System.out.println(idades1[0]);
+		System.out.println(idades1[1]);
+		System.out.println(idades1[2]);
+		
+		boolean[] idades2 = new boolean[3];
+		System.out.println(idades2[0]);
+		System.out.println(idades2[1]);
+		System.out.println(idades2[2]);
+		
+		String[] idades3 = new String[3];
+		System.out.println(idades3[0]);
+		System.out.println(idades3[1]);
+		System.out.println(idades3[2]);
+		
+	}
+
+}
