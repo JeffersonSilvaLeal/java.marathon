3edda813c226795157d8487e0561bfef4689eca6
JefferonSilva <drankhar@gmail.com>
2022-03-30 17:12:45 -0300

    Arrays pt 01

---
+++ src/maratona/java/arrays/Aula07Arrays01.java
@@ -0,0 +1,19 @@
+package maratona.java.arrays;
+
+public class Aula07Arrays01 {
+
+	public static void main(String[] args) {
+		/**
+		 * Arrays são do tipo Reference
+		 */
+		int[] idades = new int[3];
+		idades[0] = 21;
+		idades[1] = 100;
+		idades[2] = 475;
+		System.out.println(idades[0]);
+		System.out.println(idades[1]);
+		System.out.println(idades[2]);
+
+	}
+
+}
