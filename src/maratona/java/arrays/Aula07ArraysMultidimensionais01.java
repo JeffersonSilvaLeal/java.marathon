f6d51b6261345098c0d82eca04390aa89eb2e76a
JefferonSilva <drankhar@gmail.com>
2022-03-31 09:58:03 -0300

    Arrays Multidimensionais pt 01

---
+++ src/maratona/java/arrays/Aula07ArraysMultidimensionais01.java
@@ -0,0 +1,36 @@
+package maratona.java.arrays;
+
+public class Aula07ArraysMultidimensionais01 {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * Arrays multidimencionais nada mais é que array de array
+		 */
+		
+		/**
+		 *  1,2,3,4,5,6,7,8,9,10,11,12
+		 *  1 - 30 dias
+		 * 
+		 */
+		
+		int[][] dias = new int[3][3];
+		
+		dias[0][0] = 31;
+		dias[0][1] = 28;
+		dias[0][2] = 31;
+	
+		dias[1][0] = 31;
+		dias[1][1] = 28;
+		dias[1][2] = 31;
+		
+
+		for (int i = 0; i < dias.length; i++) {
+			for (int j = 0; j < dias[0].length; j++) {
+				System.out.println(dias[i][j]);
+			}
+		}
+	
+	}
+
+}
