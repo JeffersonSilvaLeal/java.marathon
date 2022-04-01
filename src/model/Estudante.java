a78b2a503c2a2d669de0f5ce5b0a3be08e673f9c
JefferonSilva <drankhar@gmail.com>
2022-04-01 15:40:05 -0300

    Introdução classes pt 01

---
+++ src/model/Estudante.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * 
+ * @author jefferson.leal
+ * Classe é a representação de um Objeto do mundo real
+ *
+ */
+public class Estudante {
+
+	public String nome;
+	public int idade;
+	public char sexo;
+	
+	
+	
+	
+}
---
+++ src/test/EstudanteTest01.java
@@ -0,0 +1,20 @@
+package test;
+
+import model.Estudante;
+
+public class EstudanteTest01 {
+
+	public static void main(String[] args) {
+		
+		
+		Estudante estudante = new Estudante();
+		estudante.nome  = "Jefferson";
+		estudante.idade = 10;
+		estudante.sexo = 'M';
+		
+		System.out.println(estudante.nome);
+		System.out.println(estudante.idade);
+		System.out.println(estudante.sexo);
+		
+	}
+}
